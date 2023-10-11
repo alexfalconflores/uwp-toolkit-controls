c534b5407523b5ff87c8ee597628e6c62b4dd2fa
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-10-11 12:04:36 -0500

    ➕ [ADD] TitleBarSize and ToastAlignment

---
+++ UWP.Toolkit.Controls/Enum/TitleBarSize.cs
@@ -0,0 +1,16 @@
+﻿namespace UWP.Toolkit.Controls.Enum;
+
+/// <summary>
+/// Enum: Size of the TitleBar.
+/// </summary>
+public enum TitleBarSize
+{
+    /// <summary>
+    /// Short. Value is 32.
+    /// </summary>
+    Short = 32,
+    /// <summary>
+    /// Tall. Value is 48.
+    /// </summary>
+    Tall = 48
+}
---
+++ UWP.Toolkit.Controls/Enum/ToastAlignment.cs
@@ -0,0 +1,14 @@
+﻿namespace UWP.Toolkit.Controls.Enum;
+
+/// <summary>
+/// Enum: Alignment of the Toast.
+/// </summary>
+public enum ToastAlignment
+{
+    TopLeft,
+    TopCenter,
+    TopRight,
+    BottomLeft,
+    BottomCenter,
+    BottomRight
+}

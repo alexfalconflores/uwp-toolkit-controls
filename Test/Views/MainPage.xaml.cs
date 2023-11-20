d33cb27f6b08a769546435e55a5b1c3e63d95f51
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-11-19 23:14:18 -0500

    [TEST] PreviewPopup Class

--- Test/Views/MainPage.xaml.cs
+++ Test/Views/MainPage.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 
 using Test.ViewModels;
-
+using UWP.Markup;
+using UWP.Toolkit.Controls;
+using UWP_Toolkit.Extensions;
+using Windows.UI;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace Test.Views
 {
@@ -13,6 +18,16 @@ namespace Test.Views
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            TextBlock texto = new TextBlock().Text("PreviewPopup");
+            PreviewPopup popup = new PreviewPopup()
+                .Content(new TextBlock().Text("Texto dentro del contenido"))
+                .ContentPadding(10);
+
+            popup.Show();
         }
     }
 }

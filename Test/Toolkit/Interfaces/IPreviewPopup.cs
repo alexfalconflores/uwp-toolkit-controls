e61c20fe3895f291ab730488ef6ba5c2d20c6bfc
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-11-19 23:09:04 -0500

    [ADD] ContentProperties, OverlayProperties

---
+++ Test/Toolkit/Interfaces/IPreviewPopup.cs
@@ -0,0 +1,30 @@
+﻿using Windows.Foundation.Metadata;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace UWP.Toolkit.Controls.Interfaces;
+
+[ExclusiveTo(typeof(PreviewPopup))]
+public interface IPreviewPopup
+{
+    bool IsOpen { get; }
+    bool IsLightDismissEnabled { get; set; }
+    Style OverlayStyle { get; set; }
+    Thickness? Padding { get; set; }
+    Brush Background { get; set; }
+
+    Style CloseButtonStyle { get; set; }
+    string CloseButtonToolTip { get; set; }
+    Visibility IsCloseButtonVisible { get; set; }
+
+    UIElement Content { get; set; }
+    Style ContentStyle { get; set; }
+    CornerRadius? ContentCornerRadius { get; set; }
+    Thickness? ContentPadding { get; set; }
+    Brush ContentBackground { get; set; }
+    Thickness? ContentBorderThickness { get; set; }
+    Brush ContentBorderBrush { get; set; }
+
+    void Show();
+    void Hide();
+}
